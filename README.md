1d2494106f0947ccdf20e707ce1ae7ea7cf05a5d
Jamesb27598 <98533827+JamesB275@users.noreply.github.com>
2022-04-04 22:23:44 +0000

    Final version

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 #### CS 110
-# Chapter 4 - Lab - Functions
-
-### [Assignment Description](https://docs.google.com/document/d/1V20D_upUX4MO8YmskKlRB25Yu2pCEv3-h8z4EAfrSno/edit?usp=sharing)
+# Midterm - Python Programming
 
 ***
 
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- program draws sine, cosine, and tangent. I couldn't figure out how to get it to clear off after every function and still keep the grid. 
+What did you clean up?:
+Got rid of some unneccesary turtle.penup() and turtle.pendown() statements. In the for loop, I fixed the range and made it (-360,361) because otherwise the 360 would be exclusive. 
 
-## GRACE DAYS
-Grace days used for this assignment: 0 
+Summary of function(s) added:
+added raiseNumber function, and evenOrOdd function.
 
-Grace days remaining: 5/5
+Summary of Feature Added:
+The raiseNumber function will ask you for a number to raise to a power, and a power to raise the number by. For example, if you put in 3 as your number to raise by a power and 5 as your power to raise your number by, it would be 3^5. The evenOrOdd function then takes this value and decides if it is even or odd. For example, 3^5 is 243, so the evenOrOdd will print that the resulting number is odd. 
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- All parts completed
-
+ no known bugs
+ 
 ## REFERENCES:
- stackexchange
+python math module library
 
 ## MISCELLANEOUS COMMENTS:
-
+none
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import math
 # then you do not fully understand functions
 # and should review how they work or ask for help
 #Part A
-def drawSineCurve(turtle):
- for radian in range(-360,360):
+def drawSineCurve(turtle=None):
+ for radian in range(-360,361):
     y = math.sin(math.radians(radian))
     turtle.goto(radian,y)
-    turtle.down()
+    turtle.pendown()
 #Part B
 def setupWindow(mywindow=None):
   mywindow=turtle.setworldcoordinates(-360,-1,360,1)
@@ -20,26 +20,31 @@ def setupAxis(myturtle):
   myturtle.goto(0,0)
   myturtle.goto(0,1)
   myturtle.goto(0,-1)
-  myturtle.up()
+  myturtle.penup()
   
-def drawCosineCurve(turtle):
-  turtle.down()
-  turtle.up()
-  for radian in range(-360,360):
+def drawCosineCurve(turtle=None):
+  turtle.penup()
+  for radian in range(-360,361):
     y = math.cos(math.radians(radian))
     turtle.goto(radian,y)
-    turtle.down()
+    turtle.pendown()
 
-def drawTangentCurve(turtle):
-  turtle.down()
-  turtle.up()
-  for radian in range(-360,360):
+def drawTangentCurve(turtle=None):
+  turtle.penup()
+  for radian in range(-360,361):
     y = math.tan(math.radians(radian))
     turtle.goto(radian,y)
-    turtle.down()
+    turtle.pendown()
 
+def raiseNumber(response=None,power=None):
+  result=math.pow(response,power)
+  return result
 
-
+def evenOrOdd(pow_result=None):
+  if (pow_result%2)==0:
+    result_number=print("The resulting number is even")
+  else:
+    result_number=print("The resulting number is odd")
 
 ##########  Do Not Alter Any Code Past Here ########
 def main():
@@ -57,5 +62,9 @@ def main():
     drawSineCurve(dart)
     drawCosineCurve(dart)
     drawTangentCurve(dart)
+    response=int(input("input a number to be raised to a power: "))
+    power=int(input("input a power to raise the number by: "))
+    pow_result=raiseNumber(response,power)
+    evenOrOdd(pow_result)
     wn.exitonclick()
 main()
